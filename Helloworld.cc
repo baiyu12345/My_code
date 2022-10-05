c6b13cf7336b7eb300607c1763ac50568f46457d
yu.bai <1623068025@qq.com>
2022-10-05 23:45:16 +0800

    baiyu_commit

--- Helloworld.cc
+++ Helloworld.cc
@@ -3,12 +3,12 @@ using namespace std;
 int main()
 {
 
-    cout<<"Hello world!! I am baiyu"<<endl;
-    cout<<"Hello world!! I am baiyu"<<endl;
-    cout<<"Hello world!! I am baiyu"<<endl;
-    cout<<"Hello world!! I am baiyu"<<endl;
-    cout<<"Hello world!! I am baiyu"<<endl;
-    cout<<"Hello world!! I am baiyu"<<endl;
+    cout<<"Hello world!! I am baiyu11"<<endl;
+    cout<<"Hello world!! I am baiyu11"<<endl;
+    cout<<"Hello world!! I am baiyu11"<<endl;
+    cout<<"Hello world!! I am baiyu11"<<endl;
+    cout<<"Hello world!! I am baiyu11"<<endl;
+    cout<<"Hello world!! I am baibaiyu11yu"<<endl;
     
     return 0;
 }
